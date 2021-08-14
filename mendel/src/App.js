3e90a08b46fbb3127f6f33bd6b0bc9d6aad8e765
tpadgett <tim_padgett@yahoo.com>
2021-08-14 16:45:42 -0700

    api call

---
+++ mendel/src/App.js
@@ -0,0 +1,66 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useEffect, useState} from 'react';
+
+function App() {
+
+  const url = 'https://randomuser.me/api/?results=20';
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setResponse(json.results);
+      console.log(response)
+      console.log(json.results) //results
+      console.log(flatten(json.results[0].location)) //flattened
+    } catch (error) {
+      console.log(error, 'error')
+    }
+  }
+
+  const flatten = (data) => {
+    var result = {};
+    function recurse(cur, prop) {
+      if (Object(cur) !== cur) {
+        result[prop] = cur;
+      } else if (Array.isArray(cur)) {
+        for (var i = 0, l = cur.length; i < l; i++)
+          recurse(cur[i], prop + "[" + i + "]");
+        if (l == 0)
+          result[prop] = [];
+      } else {
+        var isEmpty = true;
+        for (var p in cur) {
+          isEmpty = false;
+          recurse(cur[p], prop ? prop + "." + p : p);
+        }
+        if (isEmpty && prop)
+          result[prop] = {};
+      }
+    }
+    recurse(data, "");
+    return result;
+  }
+
+  const flattenedObject = (object, level) => {
+    object.map((entry) => {
+      return flatten(entry)
+    })
+  }
+
+  const [response, setResponse] = useState()
+  const [data, setData] = useState([]);
+
+  return (
+    <div className="App">
+      {console.log(response)}
+    </div>
+  );
+}
+
+export default App;
